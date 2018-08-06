3033f5a232eaedb4ab45dd2e2737797f6265135c
Julio Lovera <lekitar123@gmail.com>
2018-08-06 19:20:16 -0400

    Resumen del perfil de freelancer
    Se termina la pagina de detalles publicos del freelancer

--- p-freelancer/perfil.php
+++ p-freelancer/perfil.php
@@ -67,3 +67,155 @@
 		</div> 
 	</div>
 </section>
+
+<section class="container-fluid" id="resumen-candidato">
+	<div class="row">
+		<div class="container" >
+			<div class="row section">
+				<div class="col-md-12 titulo-resumen">
+					<h3>About Resume</h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12 descripcion-resumen">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia dolore adipisci doloribus porro molestiae, perspiciatis nemo vero totam voluptas sit eligendi iure, temporibus maxime quas. Quae, explicabo et culpa dolorem!
+
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, nemo, delectus? Rerum officiis quisquam esse iusto officia illo facilis commodi, perferendis maiores quas vero blanditiis, corporis veritatis deserunt debitis. Vitae.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia dolore adipisci doloribus porro molestiae, perspiciatis nemo vero totam voluptas sit eligendi iure, temporibus maxime quas. Quae, explicabo et culpa dolorem!
+
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, nemo, delectus? Rerum officiis quisquam esse iusto officia illo facilis commodi, perferendis maiores quas vero blanditiis, corporis veritatis deserunt debitis. Vitae.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="container">
+			<div class="row section">
+				<div class="col-md-12 titulo-resumen">
+					<h3>Educación</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 descripcion-resumen">
+					<p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, maiores excepturi sint placeat veniam dolores eum tempora cum soluta amet, vitae nihil tenetur, cumque at accusamus id quod magnam aut.</span><span>Placeat alias voluptate dolores, totam maxime. Sit odio molestias blanditiis eum iure architecto eaque maiores, totam quae sequi, aperiam repellat obcaecati soluta adipisci quisquam rerum quidem, deleniti illum nesciunt! Repudiandae.</span></p>
+					<ul>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde sit laborum dolor mollitia a, suscipit fugit eveniet aut totam, dicta, rem enim. Odit suscipit, ad laborum maxime dolores a tempore.
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores incidunt inventore doloribus, ut ratione sed explicabo qui accusantium eveniet optio veritatis sequi, quasi esse, in vel amet eum fugiat, culpa?
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti eligendi nostrum, temporibus consectetur eum fuga, amet inventore laboriosam hic quibusdam dignissimos adipisci, repellendus placeat rerum accusamus architecto assumenda ipsa ad.
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus explicabo repellendus, officia recusandae minus neque beatae quasi, eius sit. At aut reiciendis natus molestias. Recusandae optio distinctio iusto aut molestias.
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eveniet quisquam accusamus possimus distinctio quis, at dolore. Quo perspiciatis incidunt, dolores nisi ipsa consequatur deserunt tempora assumenda architecto aspernatur cumque!
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro nemo quisquam blanditiis quasi, velit voluptas molestiae ut tenetur recusandae labore maxime esse, at totam sapiente voluptatibus voluptate dignissimos doloremque amet?
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, error, assumenda nihil ad est possimus, velit qui maiores non cumque, accusamus rerum asperiores aliquid. Illo adipisci minus enim, natus suscipit.
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit corrupti ipsa, sequi. Nesciunt dolores ad aliquam, maiores quibusdam quo velit commodi nisi, ducimus. Dolore quasi facere veritatis officiis impedit atque!
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio laborum aliquam eligendi, beatae, enim aut, facere nemo quaerat distinctio atque fuga, soluta quidem! Facere praesentium distinctio sunt. Dolor excepturi, iusto.
+						</li>
+						<li> 
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia fugit saepe labore soluta aspernatur, velit obcaecati rem quibusdam accusamus natus vitae unde quas, ea ratione, maxime sint est quos voluptates!
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="container">
+			<div class="row section">
+				<div class="col-md-12 titulo-resumen">
+					<h3>Skills</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 descripcion-resumen">
+					<p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, maiores excepturi sint placeat veniam dolores eum tempora cum soluta amet, vitae nihil tenetur, cumque at accusamus id quod magnam aut.</span><span>Placeat alias voluptate dolores, totam maxime. Sit odio molestias blanditiis eum iure architecto eaque maiores, totam quae sequi, aperiam repellat obcaecati soluta adipisci quisquam rerum quidem, deleniti illum nesciunt! Repudiandae.</span></p>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-6 col-lg-4">
+						<span> Desarrollo web</span>
+						<div class="progress">
+							<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+								40%
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
