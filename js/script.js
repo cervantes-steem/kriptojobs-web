b4a48716e761e26e85a4b6b0709396df63cb5d61
Julio Lovera <lekitar123@gmail.com>
2018-08-17 16:00:35 -0400

    Se agrega pagina de busqueda para los empleadores

--- js/script.js
+++ js/script.js
@@ -51,6 +51,7 @@ $('.wrapper').on('click', '.remove', function() {
 });
 $('.wrapper').on('click', '.clone', function() {
   $('.clone').closest('.wrapper').find('.element').first().clone().appendTo('.results');
+   $('.clone').closest('.wrapper').find('.clone').not(':first').last().remove();
 });
 var editor = new wysihtml5.Editor("some-textarea2", { // id of textarea element
   toolbar:      "wysihtml5-toolbar2", // id of toolbar element
@@ -68,3 +69,47 @@ $('#rangofechasestudio .time-daterange').datepicker({
 
 
 /* Fin Elementos que se deben clonar */
+
+/* Elementos que clonan el html */
+$('.wrapper-experiencia').on('click', '.remove-experiencia', function() {
+  $('.remove-experiencia').closest('.wrapper-experiencia').find('.element-experiencia').not(':first').last().remove();
+});
+$('.wrapper-experiencia').on('click', '.clone-experiencia', function() {
+  $('.clone-experiencia').closest('.wrapper-experiencia').find('.element-experiencia').first().clone().appendTo('.results-experiencia');
+  $('.clone-experiencia').closest('.wrapper-experiencia').find('.clone-experiencia').not(':first').last().remove();
+});
+var editor = new wysihtml5.Editor("some-textarea-experiencia", { // id of textarea element
+  toolbar:      "wysihtml5-toolbar-experiencia", // id of toolbar element
+  parserRules:  wysihtmlParserRules // defined in parser rules set 
+});
+
+$('#rangofechasestudio-experiencia .time-daterange').datepicker({
+    startView: 2,
+    minViewMode: 1,
+    clearBtn: true,
+    language: "es",
+    autoclose: true,
+    toggleActive: true
+});
+
+
+/* Fin Elementos que se deben clonar */
+
+
+$('.wrapper-idioma').on('click', '.remove-idioma', function() {
+  $('.remove-idioma').closest('.wrapper-idioma').find('.element-idioma').not(':first').last().remove();
+});
+$('.wrapper-idioma').on('click', '.clone-idioma', function() {
+  $('.clone-idioma').closest('.wrapper-idioma').find('.element-idioma').first().clone().appendTo('.results-idioma');
+  $('.clone-idioma').closest('.wrapper-idioma').find('.clone-idioma').not(':first').last().remove();
+});
+
+$('.wrapper-habilidades').on('click', '.remove-habilidades', function() {
+  $('.remove-habilidades').closest('.wrapper-habilidades').find('.element-habilidades').not(':first').last().remove();
+});
+$('.wrapper-habilidades').on('click', '.clone-habilidades', function() {
+  $('.clone-habilidades').closest('.wrapper-habilidades').find('.element-habilidades').first().clone().appendTo('.results-habilidades');
+  $('.clone-habilidades').closest('.wrapper-habilidades').find('.clone-habilidades').not(':first').last().remove();
+});
+
+    
--- p-freelancer/carga.php
+++ p-freelancer/carga.php
@@ -290,14 +290,14 @@
 						<div class="form-group"> 
 							<div class="col-md-12 margin-botton">
 								
-									
-									<input id="nombre-instituto" type="text" class="form-control" name="nombre-instituto" placeholder="Nombre de la escuela, por ejemplo: Universidad Central de Venezuela">  <!-- Este Id debe ser unico --> 
+
+								<input id="nombre-instituto" type="text" class="form-control" name="nombre-instituto" placeholder="Nombre de la escuela, por ejemplo: Universidad Central de Venezuela">  <!-- Este Id debe ser unico --> 
 								
 							</div>
 
 							<div class="col-md-12 margin-botton">
 								<!-- Este Id debe ser unico --> 
-									<input id="titulo" type="text" class="form-control" name="titulo" placeholder="Calificación, por ejemplo, Lic. en Ciencias de la computación">
+								<input id="titulo" type="text" class="form-control" name="titulo" placeholder="Calificación, por ejemplo, Lic. en Ciencias de la computación">
 								
 							</div>
 
@@ -378,15 +378,208 @@
 							</ul>
 						</div>
 
-						<textarea id="some-textarea2" class="form-control k-textbox text-area"  placeholder="Enter text ..." ></textarea>  <!-- Este Id debe ser unico --> 
+						<textarea id="some-textarea2" class="form-control k-textbox text-area margin-botton"  placeholder="Enter text ..." ></textarea>  <!-- Este Id debe ser unico --> 
 					</form>
-
+					<div class="buttons col-md-12 margin-botton">
+						<button class="clone pull-left btn btn-success">clone</button>
+						<button class="remove pull-right btn btn-danger">remove</button>
+					</div>
 				</div> <!-- Fin elemento que se clona --> 
 				<div class="results"></div> <!-- Aqui se muestra el elemento clonado--> 
-				<div class="buttons col-md-12">
-					<button class="clone pull-left btn btn-success">clone</button>
-					<button class="remove pull-right btn btn-danger">remove</button>
+				
+
+				
+			</div>
+		</div>
+
+		<div class="row wrapper-experiencia">
+			<div class="container">
+				<div class="row section">
+					<div class="col-md-12 titulo-resumen">
+						<h3>Agregar Experiencia Laboral</h3>
+					</div>
 				</div>
+				
+				<div class="row element-experiencia margin-botton"> <!-- Elemento que se clona --> 
+					<form class=" form-horizontal">
+						<div class="form-group"> 
+							<div class="col-md-12 margin-botton">
+								
+
+								<input id="nombre-empresa" type="text" class="form-control" name="nombre-empresa" placeholder="Nombre de la Empresa, por ejemplo: KriptoJobs C.A">  <!-- Este Id debe ser unico --> 
+								
+							</div>
+
+							<div class="col-md-12 margin-botton">
+								<!-- Este Id debe ser unico --> 
+								<input id="cargo" type="text" class="form-control" name="cargo" placeholder="Cargo, por ejemplo, Desarrollador Web">
+								
+							</div>
+
+							<div class="col-md-12" id="rangofechasestudio-experiencia">  <!-- Este Id debe ser unico --> 
+								<div class="input-daterange input-group time-daterange" id="datepicker">
+									<input type="text" class="input-sm form-control" name="start" />
+									<span class="input-group-addon">to</span>
+									<input type="text" class="input-sm form-control" name="end" />
+								</div>
+							</div>
+						</div>
+
+						<div id="wysihtml5-toolbar-experiencia" style="display: none;"> <!-- Este Id debe ser unico --> 
+
+							<ul class="wysihtml5-toolbar" style="">
+								<li class="dropdown">
+									<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-font"></i>&nbsp;<span class="current-font">Normal text</span>&nbsp;<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li>
+											<a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="div" >Normal text</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1" >Heading 1</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" >Heading 2</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h3" >Heading 3</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h4" >Heading 4</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h5" >Heading 5</a>
+										</li>
+										<li><a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h6" >Heading 6</a>
+										</li>
+									</ul>
+								</li>
+
+
+								<li>
+									<div class="btn-group">
+										<a class="btn  btn-default" data-wysihtml5-command="bold" title="CTRL+B"  > Bold</a>
+										<a class="btn  btn-default" data-wysihtml5-command="italic" title="CTRL+I" " >Italic</a>
+										<a class="btn  btn-default" style="text-decoration: underline;" data-wysihtml5-command="underline" title="CTRL+U"  > Underline</a>
+
+									</div>
+								</li>
+								<li>
+									<a class="btn  btn-default" data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="blockquote" data-wysihtml5-display-format-name="false" >
+
+										<span class="fa fa-quote-left"></span>
+
+									</a>
+								</li>
+								<li>
+									<div class="btn-group">
+										<a class="btn  btn-default" data-wysihtml5-command="insertUnorderedList" title=""  >
+
+											<span class="fa fa-list-ul"></span>
+
+										</a>
+										<a class="btn  btn-default" data-wysihtml5-command="insertOrderedList" title="" >
+
+											<span class="fa fa-list-ol"></span>
+
+										</a>
+										<a class="btn  btn-default" data-wysihtml5-command="Outdent" title=""  >
+
+											<span class="fa fa-outdent"></span>
+
+										</a>
+										<a class="btn  btn-default" data-wysihtml5-command="Indent" title=""  >
+
+											<span class="fa fa-indent"></span>
+
+										</a>
+									</div>
+								</li>
+							</ul>
+						</div>
+
+						<textarea id="some-textarea-experiencia" class="form-control k-textbox text-area margin-botton"  placeholder="Enter text ..." ></textarea>  <!-- Este Id debe ser unico --> 
+					</form>
+					<div class="buttons-experiencia col-md-12">
+						<button class="clone-experiencia pull-left btn btn-success">clone</button>
+						<button class="remove-experiencia pull-right btn btn-danger">remove</button>
+					</div>
+				</div> <!-- Fin elemento que se clona --> 
+				<div class="results-experiencia"></div> <!-- Aqui se muestra el elemento clonado--> 
+				
+
+				
+			</div>
+		</div>
+
+		<div class="row wrapper-idioma">
+			<div class="container">
+				<div class="row section">
+					<div class="col-md-12 titulo-resumen">
+						<h3>Idiomas</h3>
+					</div>
+				</div>
+				
+				<div class="row element-idioma margin-botton"> <!-- Elemento que se clona --> 
+					<form class=" form-horizontal">
+						<div class="form-group"> 
+							<div class="col-md-12 margin-botton">
+								<input id="idioma" type="text" class="form-control" name="idioma" placeholder="Idioma, por ejemplo: Español">  <!-- Este Id debe ser unico --> 
+							</div>
+
+							<div class="col-md-12 margin-botton">
+								<!-- Este Id debe ser unico --> 
+									<div class="input-group col-md-2">
+									<span class="input-group-addon"><i class="fas fa-language"></i></span>
+									<input id="porcentaje-idioma" type="text" class="form-control " name="porcentaje-idioma" placeholder="85 %">
+								</div>
+
+								
+								
+							</div>
+
+						</div>
+
+					</form>
+					<div class="buttons-idioma col-md-12">
+						<button class="clone-idioma pull-left btn btn-success">clone</button>
+						<button class="remove-idioma pull-right btn btn-danger">remove</button>
+					</div>
+				</div> <!-- Fin elemento que se clona --> 
+				<div class="results-idioma"></div> <!-- Aqui se muestra el elemento clonado--> 
+			
+				
+			</div>
+		</div>
+		<div class="row wrapper-habilidades">
+			<div class="container">
+				<div class="row section">
+					<div class="col-md-12 titulo-resumen">
+						<h3>Idiomas</h3>
+					</div>
+				</div>
+				
+				<div class="row element-habilidades margin-botton"> <!-- Elemento que se clona --> 
+					<form class=" form-horizontal">
+						<div class="form-group"> 
+							<div class="col-md-12 margin-botton">
+								<input id="habilidades" type="text" class="form-control" name="habilidades" placeholder="habilidades, por ejemplo: Git">  <!-- Este Id debe ser unico --> 
+							</div>
+
+							<div class="col-md-12 margin-botton">
+								<!-- Este Id debe ser unico --> 
+									<div class="input-group col-md-2">
+									<span class="input-group-addon"><i class="fas fa-language"></i></span>
+									<input id="porcentaje-habilidades" type="text" class="form-control " name="porcentaje-habilidades" placeholder="85 %">
+								</div>
+								
+							</div>
+
+						</div>
+
+					</form>
+					<div class="buttons-habilidades col-md-12">
+						<button class="clone-habilidades pull-left btn btn-success">clone</button>
+						<button class="remove-habilidades pull-right btn btn-danger">remove</button>
+					</div>
+				</div> <!-- Fin elemento que se clona --> 
+				<div class="results-habilidades"></div> <!-- Aqui se muestra el elemento clonado--> 
+				
 
 				
 			</div>
